0f1cda01767d33105f1bce1790f0840474315a2a
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2023-10-06 02:43:01 -1200

    hola

--- src/assets/carsAPI.js
+++ src/assets/carsAPI.js
@@ -17,5 +17,78 @@ export const cars = [
         description: "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
         imgUrl: "/icon-luxury.svg",
         theme: "VeryDarkCyan"
+    },
+
+
+    {
+        title: "Sports",
+        description: "Drive a car that's built for speed and performance.",imgUrl: "/icon-luxury.svg",
+        theme: "Red"
+    },
+    {
+        title: "Sedan",
+        description: "Get a comfortable and stylish car that's perfect for everyday driving.",
+        imgUrl: "/icon-luxury.svg",
+        theme: "Violet"
+    },
+    {
+        title: "Minivan",
+        description: "Get a spacious and comfortable car that's perfect for families and people who need a lot of cargo space.",
+        imgUrl: "/icon-luxury.svg",
+        theme: "Pink"
+    },
+    {
+        title: "Truck",
+        description: "Get a powerful and versatile car that's perfect for work and play.",
+        imgUrl: "/icon-luxury.svg",
+        theme: "BrightOrange"
+    },
+    {
+        title: "Electric Vehicle",
+        description: "Get a car that's good for the environment and that can save you money on gas.",
+        imgUrl: "/icon-luxury.svg",
+        theme: "DarkCyan"
+    },
+    {
+        title: "Hybrid Vehicle",
+        description: "Get a car that gets better gas mileage and produces fewer emissions than a traditional gasoline-powered car.",
+        imgUrl: "/icon-luxury.svg",
+        theme: "VeryDarkCyan"
+    },
+    {
+        title: "Luxury",
+        description: "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+        imgUrl: "/icon-luxury.svg",
+        theme: "Red"
+    },
+    {
+        title: "Very Luxury Car",
+        description: "Get a car that's all about comfort, style, and performance.",
+        imgUrl: "/icon-luxury.svg",
+        theme: "Violet"
+    },
+    {
+        title: "Classic Car",
+        description: "Get a car that's a timeless piece of automotive history.",
+        imgUrl: "/icon-luxury.svg",
+        theme: "Pink"
+    },
+    {
+        title: "Muscle Car",
+        description: "Get a car that's all about power and performance.",
+        imgUrl: "/icon-luxury.svg",
+        theme: "BrightOrange"
+    },
+    {
+        title: "Deluxe",
+        description: "Get a car that's all about comfort, style, and performance.",
+        imgUrl: "/icon-luxury.svg",
+        theme: "DarkCyan"
+    },
+    {
+        title: "Family",
+        description: "Get a spacious and versatile car that's perfect for families and outdoor adventures.",
+        imgUrl: "/icon-luxury.svg",
+        theme: "VeryDarkCyan"
     }
 ]
